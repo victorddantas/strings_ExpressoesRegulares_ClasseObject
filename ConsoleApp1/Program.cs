bca62047cea39b8214e80fd4e47ebdcd4645ee4f
Victor Domingues Dantas <vic.d.dantas61@gmail.com>
2023-02-05 17:35:57 -0300

    Método substring

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,13 +11,20 @@ namespace SistemaAgencia
     {
         static void Main(string[] args)
         {
-            //Trabalhando com string
+           
+           // metodoSubstring();
+      
+        }
+
+        #region Trabalhando com strings
+        private static void metodoSubstring()
+        {
 
             //Método substring - possibilita obtermos um trecho de uma string através do índice, criando uma nova string com o trecho 
 
             string url = "pagina?argumentos";
-            
-            Console.WriteLine(url); 
+
+            Console.WriteLine(url);
 
             string url2 = url.Substring(6); //criando uma nova string pois uma string é imutável(obtendo o trecho da string a partir do indice 6 incluindo o próprio indice 6).
 
@@ -25,9 +32,13 @@ namespace SistemaAgencia
 
             url += "teste"; //nesse caso será incluio a palavra teste a primeira url, porém na exibição será criada uma nova string temporária, pois a string original permance imutável. 
 
-            Console.WriteLine(url); 
+            Console.WriteLine(url);
 
             Console.ReadLine();
         }
+
+
+
+        #endregion
     }
 }
