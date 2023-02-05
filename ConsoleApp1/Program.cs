00a749ebc7edf387e8d63faa464a47ef19f59c60
Victor Domingues Dantas <vic.d.dantas61@gmail.com>
2023-02-05 16:24:30 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAgencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Trabalhando com string
+
+            //Método substring - possibilita obtermos um trecho de uma string através do índice, criando uma nova string com o trecho 
+
+            string url = "pagina?argumentos";
+            
+            Console.WriteLine(url); 
+
+            string url2 = url.Substring(6); //criando uma nova string pois uma string é imutável(obtendo o trecho da string a partir do indice 6 incluindo o próprio indice 6).
+
+            Console.WriteLine(url2);
+
+            url += "teste"; //nesse caso será incluio a palavra teste a primeira url, porém na exibição será criada uma nova string temporária, pois a string original permance imutável. 
+
+            Console.WriteLine(url); 
+
+            Console.ReadLine();
+        }
+    }
+}
