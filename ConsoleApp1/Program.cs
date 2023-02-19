dc0512a454bc4968c29440f97bfa596e2859e7d2
Victor Domingues Dantas <vic.d.dantas61@gmail.com>
2023-02-19 17:20:14 -0300

    Utilizando o Regex

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SistemaAgencia
@@ -16,7 +17,7 @@ namespace SistemaAgencia
             //exemploMetodoSubstring();
             //utilizandoExtratorDeUrl();
             //verificandoSeExisteUmTermoNaString();
-
+            exemploDeExpressaoRegular();
 
         }
 
@@ -67,6 +68,32 @@ namespace SistemaAgencia
             Console.WriteLine(url.StartsWith("https://www.google.com/")); //utilizando o método StartsWith para verificar se uma string inicia com determinado termo
 
             Console.WriteLine(url.EndsWith("Dolar"));//utilizando o método EndsWith para verificar se uma string termina com determinado termo
+
+            Console.ReadLine();
+        }
+
+        private static void exemploDeExpressaoRegular()
+        {
+            //Uma expressão regular é uma expressão que criamos com uma determinada, onde um termo definido a partir dessa expressão deve respeitar essas regras definidas nessa expressão 
+
+
+            //podemos definir uma expressão regular por exemplo para o padrão de escrita de um número de telefone 
+
+            string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+
+            //Um número de telefone deve respeitar as seguintes regras impostas na expressão acima (8 caracteres de 0 a 9 separados po um hifen) 
+
+            //Utilizando a classe regex podemos recuperar expressões regulares 
+
+            //Exemplo: Verificar se os dados de uma string é compátivel com a expressão regular definida em "padrao".
+
+            string textoDeTeste = "Meu número de telefone é 9999-9999";
+
+            Console.WriteLine(Regex.IsMatch(textoDeTeste, padrao)); //O IsMatch é um método do regex que permite verificar se no texto proposto exite um termo que repeite a regra proposta na expressão regular
+
+            Match resultado = Regex.Match(textoDeTeste, padrao);  //O Match retrona o valor que respeita a regra proposta na expressão regular
+
+            Console.WriteLine(resultado.Value);
 
             Console.ReadLine();
         }
